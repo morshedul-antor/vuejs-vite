ecb4c7f81992fad1cca6f3aea63819ec85de8412
morshedul-antor <antor.morshedul@gmail.com>
2024-08-27 14:58:50 +0600

    feat: about component add

---
+++ src/components/About/About.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <div>
+        <p>About Page!</p>
+        <RouterLink to="/">Goto Home Page</RouterLink>
+    </div>
+</template>
