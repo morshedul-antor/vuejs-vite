710d5f11bfb5d29ea87d6200958ce3e1a4bda568
morshedul-antor <antor.morshedul@gmail.com>
2024-08-27 15:01:06 +0600

    feat: vue router setup

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import { createRouter, createWebHistory } from 'vue-router'
+import { Home, About } from './components'
+
+// Define your routes
+const routes = [
+    { path: '/', component: Home, props: { msg: 'Hello from router!' } },
+    { path: '/about', component: About }
+]
+
+// Create the router instance
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+export default {
+    name: 'App',
+    router
+}
+</script>
+
+<!-- App -->
+<template>
+    <router-view></router-view>
+</template>
